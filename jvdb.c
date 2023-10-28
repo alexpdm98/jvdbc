d9381f6ce9d5d3bf81996a018ca74459cbc32d8a
alexpdm98 <96046053+alexpdm98@users.noreply.github.com>
2023-10-28 16:02:42 +0200

    añadir

--- jvdb.c
+++ jvdb.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char* argv[]){
-FILE *archivo;
-archivo = fopen("basededatos.txt", "w");
-char *texto = "JVDB";
-fputs(texto,archivo);
-fclose(archivo);
+    FILE *archivo;
+    char *basededatos = argv[1];
+    char *nombrearchivo = argv[2];
+    char *texto = argv[3];
+    char ruta[100];
+    strcpy(ruta,basededatos);
+    strcat(ruta,"-");
+    strcat(ruta,nombrearchivo);
+    strcat(ruta,".txt");
+    archivo = fopen(ruta, "a");
+    fputs(strcat(texto,"\n"),archivo);
+    fclose(archivo);
 return 0;
 }
